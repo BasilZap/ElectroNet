a92c2daa8cd5b71f423c27f20afc6055d9ff0555
BasilZap <zapr9@yandex.ru>
2023-12-31 17:32:55 +0300

    В админку добавлены отображения уззлов сети и продуктов

--- electronet/admin.py
+++ electronet/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from electronet.models import Product, Company
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'launch_date')
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'country', 'city', 'street', 'building', 'distributor', 'debt',
+                    'creation_date')
